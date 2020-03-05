1b277efeda2caf16c263fa163661792c1abe01f4
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-05 01:27:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
-end
+  
+  if array.length == 1
+    array.first 
+  elsif array.length == 2 
+    array.join(" and ") 
+  elsif array.length >= 3
+    array[array.length - 1] = "and #{array[array.length - 1]}"
+    array.join(", ")
+  end
+  
+end  
+  
