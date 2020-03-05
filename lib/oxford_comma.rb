c95eb23ff20935c0361c91b47fa195594ffe6a5a
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-05 01:02:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,15 @@ def oxford_comma(array)
   return array.first if array.length == 1
   return array.join(" and ") if array.length == 2
   
-  array_without_last_ele = array.slice(0, array.length -1)
-  separeted_by_commas = array_without_last_ele.split(",")
-  
+   
+ num = array.length - 1
+ array_without_last_ele = array.slice(0, num)
+puts array_without_last_ele
+
+separeted_by_commas = array_without_last_ele.join(",")
+puts separeted_by_commas
+separeted_by_commas += " and #{array[array.size]}"
+puts separeted_by_commas
   
   
   
