41c6a7a3999ecb9ad6fff0b53c56d5b18c0bd0b1
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-05 01:22:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,8 @@ def oxford_comma(array)
   return array.first if array.length == 1
   return array.join(" and ") if array.length == 2
   
-   
+  if array.length >= 3
+    array[array.length - 1] = "and #{array[array.length - 1]}"
+    return array.split(",")
+  end
   
-  
-  
-  return array.sort if array.length > 3
-  
-end
